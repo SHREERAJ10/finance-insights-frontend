f22384558d0412a827f82b27ff65dedcd3661bcc
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-12-20 19:06:36 +0545

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Auth from "./components/Auth.jsx"
+import Login from "./pages/Login.jsx"
+import SignUp from "./pages/SignUp.jsx"
+
+function App() {
+
+  return (
+    <div className="font-urbanist">
+      <SignUp />
+    </div>
+  )
+}
+
+export default App
